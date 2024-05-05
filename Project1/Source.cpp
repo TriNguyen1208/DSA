38eae5db5e986375556239aa0aa7b1bb2021c476
Nguyen Duc Tri <ductri0981@gmail.com>
2024-05-05 18:15:48 +0700

    Bai 21

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -2,28 +2,43 @@
 #include<string>
 #include<string.h>
 #include<vector>
+#include<set>
+#include<sstream>
 using namespace std;
 
+void ChangeToLower(string& word)
+{
+	for (int i = 0; i < word.length(); i++)
+	{
+		word[i] = tolower(word[i]);
+	}
+}
 int main()
 {
-	string s;
-	getline(cin, s);
-	//python
-	string t = "python";
-	int idx = 0;
-	for (int i = 0; i < s.length(); i++)
+	string S;
+	getline(cin, S);
+	string T;
+	getline(cin, T);
+	ChangeToLower(S);
+	ChangeToLower(T);
+	set<string>s;
+	set<string>t;
+	stringstream ss(S);
+	stringstream st(T);
+	string word;
+	while (ss >> word)
 	{
-		if (s[i] == t[idx])
+		s.insert(word);
+	}
+	while (st >> word)
+	{
+		t.insert(word);
+	}
+	for (set<string>::iterator it = s.begin(); it != s.end(); it++)
+	{
+		if (t.find(*it) != t.end())
 		{
-			idx++;
+			cout << *it << " ";
 		}
-	}
-	if (idx == 6)
-	{
-		cout << "YES";
-	}
-	else
-	{
-		cout << "NO";
 	}
 }
