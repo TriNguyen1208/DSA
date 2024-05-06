117d522444e3bd0268261e1fcb24ecc0196509e6
Nguyen Duc Tri <ductri0981@gmail.com>
2024-05-07 00:02:18 +0700

    Bai 20 2 chieu DSA

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -18,18 +18,19 @@ void InputArray(int a[10][10], int& n, int& m)
 		}
 	}
 }
+
+
+
 int dx[4] = { -1, 0, 0, 1 };
 int dy[4] = { 0, -1, 1, 0 };
 void Loang(int a[10][10], int n, int m, int i, int j)
 {
 	a[i][j] = 0;
-	cout << "Di qua " << i << " " << j << endl;
 	for (int k = 0; k < 4; k++)
 	{
 		if (i + dx[k] >= 0 && i + dx[k] < n && j + dy[k] >= 0 && j + dy[k] < m && a[i + dx[k]][j + dy[k]] == 1)
 		{
 			Loang(a, n, m, i + dx[k], j + dy[k]);
-			a[i][j] = 1;
 		}
 	}
 }
@@ -37,18 +38,13 @@ int main()
 {
 	int a[10][10], n, m;
 	InputArray(a, n, m);
-	int cnt = 0;
-	for (int i = 0; i < n; i++)
+	Loang(a, n, m, 0, 0);
+	if (a[2][4] == 0)
 	{
-		for (int j = 0; j < m; j++)
-		{
-			if (a[i][j] == 1)
-			{
-				cnt++;
-				Loang(a, n, m, i, j);
-				cout << endl;
-			}
-		}
+		cout << "YES";
 	}
-	cout << endl;
+	else
+	{
+		cout << "NO";
+	}
 }
