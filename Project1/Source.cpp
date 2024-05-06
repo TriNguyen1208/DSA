211dd5bfa4bdab76188df92bae10c341054fa78e
Nguyen Duc Tri <ductri0981@gmail.com>
2024-05-07 01:03:22 +0700

    Bai 26 mang 2 chieu dsa

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -1,55 +1,49 @@
 #include<iostream>
-#include<string>
-#include<string.h>
-#include<vector>
-#include<set>
-#include<sstream>
-#include<algorithm>
 using namespace std;
 
-void InputArray(int a[10][10], int& n, int& m)
+void InputArray(int a[10][10], int& N)
 {
-	cin >> n >> m;
-	for (int i = 0; i < n; i++)
+	cin >> N;
+	for (int i = 0; i < N; i++)
 	{
-		for (int j = 0; j < m; j++)
+		for (int j = 0; j < N; j++)
 		{
 			cin >> a[i][j];
 		}
 	}
 }
-int dx[4] = { -1, 0, 0, 1 };
-int dy[4] = { 0, -1, 1, 0 };
-void Loang(int a[10][10], int n, int m, int i, int j, int& count)
+int dy[3] = { -1, 0, 1 };
+void MaximumPath(int a[10][10], int tmp[10][10], int N)
 {
-	a[i][j] = 0;
-	for (int k = 0; k < 4; k++)
+	for (int i = 0; i < N; i++)
 	{
-		int i1 = i + dx[k], j1 = j + dy[k];
-		if (i1 >= 0 && i1 < n && j1 >= 0 && j1 < m && a[i1][j1] == 1)
+		tmp[0][i] = a[0][i];
+	}
+	for (int i = 1; i < N; i++)
+	{
+		for (int j = 0; j < N; j++)
 		{
-			count++;
-			Loang(a, n, m, i1, j1, count);
+			int Max = INT_MIN;
+			for (int k = 0; k < 3; k++)
+			{
+				if (j + dy[k] >= 0 && j + dy[k] < N)
+				{
+					Max = max(Max, a[i - 1][j + dy[k]]);
+				}
+			}
+			tmp[i][j] = a[i][j] + Max;
 		}
 	}
 }
 int main()
 {
-	int a[10][10], n, m;
-	InputArray(a, n, m);
+	int a[10][10], tmp[10][10], N;
+	InputArray(a, N);
+	MaximumPath(a, tmp, N);
 	int Max = INT_MIN;
-	for (int i = 0; i < n; i++)
+	for (int i = 0; i < N; i++)
 	{
-		for (int j = 0; j < m; j++)
-		{
-			if (a[i][j] == 1)
-			{
-				int count = 1;
-				Loang(a, n, m, i, j, count);
-				Max = max(Max, count);
-			}
-		}
+		Max = max(Max, tmp[N - 1][i]);
 	}
 	cout << Max;
 }
-
