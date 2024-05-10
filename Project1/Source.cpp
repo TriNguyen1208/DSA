5afc40d82e071c96005dc32e7c479d4b9a40e11d
Nguyen Duc Tri <ductri0981@gmail.com>
2024-05-10 14:50:26 +0700

    Tong lon nhat nho nhat dung backtracking

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -2,17 +2,71 @@
 #include<algorithm>
 #include<vector>
 #include<set>
-#include<iomanip>
 using namespace std;
 
-bool check(int a[], int n, int k, double maxWire)
+int sum(int a[], int n)
 {
 	int res = 0;
 	for (int i = 0; i < n; i++)
 	{
-		res = res + (int)a[i] / maxWire;
+		res = res + a[i];
 	}
-	return res >= k;
+	return res;
+}
+int compare(int a[], int n, int k, int maxSum)
+{
+	int cnt = 0;
+	int sum = 0;
+	for (int i = 0; i < n; i++)
+	{
+		if (sum + a[i] > maxSum)
+		{
+			cnt++;
+			sum = 0;
+		}
+		sum = sum + a[i];
+	}
+	cnt++;
+	if (cnt > k) //lower
+	{
+		return 1;
+	}
+	else//greater
+	{
+		return 0;
+	}
+}
+void BackTrack(int a[], int partition[], int n, int k, int& max_min, int start = 0, int current = 0)
+{
+	if (start == n)
+	{
+		for (int i = 0; i < k; i++)
+		{
+			if (partition[i] == 0)
+			{
+				return;
+			}
+		}
+		int sum[100] = { 0 };
+		int j = 0;
+		for (int i = 0; i < k; i++)
+		{
+			int k = j;
+			for (; k < j + partition[i]; k++)
+			{
+				sum[i] = sum[i] + a[k];
+			}
+			j = k;
+		}
+		max_min = min(max_min, *max_element(sum, sum + k));
+		return;
+	}
+	for (int i = current; i < k; i++)
+	{
+		partition[i]++;
+		BackTrack(a, partition, n, k, max_min, start + 1, i);
+		partition[i]--;
+	}
 }
 int main()
 {
@@ -22,21 +76,8 @@ int main()
 	{
 		cin >> a[i];
 	}
-	double left = 0, right = *max_element(a, a + n) * k;
-	double res = 0;
-	for(int i = 0;i < 60;i++)
-	{
-		double mid = (left + right) / 2;
-		if (check(a, n, k, mid) == true)
-		{
-			res = mid;
-			left = mid;
-		}
-		else
-		{
-			right = mid;
-		}
-	}
-	cout << setprecision(6) << fixed << res;
-	return 0;
+	int partition[100] = { 0 };
+	int max_min = INT_MAX;
+	BackTrack(a, partition, n, k, max_min);
+	cout << max_min;
 }
