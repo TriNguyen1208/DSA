ad6dab05f312dd9305eed6ce7d53e3dac613a190
Nguyen Duc Tri <ductri0981@gmail.com>
2024-05-11 13:19:57 +0700

    Tat ca bit

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -4,80 +4,58 @@
 #include<set>
 using namespace std;
 
-int sum(int a[], int n)
-{
-	int res = 0;
-	for (int i = 0; i < n; i++)
-	{
-		res = res + a[i];
-	}
-	return res;
-}
-int compare(int a[], int n, int k, int maxSum)
-{
-	int cnt = 0;
-	int sum = 0;
-	for (int i = 0; i < n; i++)
-	{
-		if (sum + a[i] > maxSum)
-		{
-			cnt++;
-			sum = 0;
-		}
-		sum = sum + a[i];
-	}
-	cnt++;
-	if (cnt > k) //lower
-	{
-		return 1;
-	}
-	else//greater
-	{
-		return 0;
-	}
-}
-void BackTrack(int a[], int partition[], int n, int k, int& max_min, int start = 0, int current = 0)
+void backtrack(int a[], int n, int start = 0)
 {
 	if (start == n)
 	{
-		for (int i = 0; i < k; i++)
+		for (int i = 0; i < n; i++)
 		{
-			if (partition[i] == 0)
-			{
-				return;
-			}
+			cout << a[i];
 		}
-		int sum[100] = { 0 };
-		int j = 0;
-		for (int i = 0; i < k; i++)
-		{
-			int k = j;
-			for (; k < j + partition[i]; k++)
-			{
-				sum[i] = sum[i] + a[k];
-			}
-			j = k;
-		}
-		max_min = min(max_min, *max_element(sum, sum + k));
+		cout << endl;
 		return;
 	}
-	for (int i = current; i < k; i++)
+	for (int i = 0; i <= 1; i++)
 	{
-		partition[i]++;
-		BackTrack(a, partition, n, k, max_min, start + 1, i);
-		partition[i]--;
+		a[start] = i;
+		backtrack(a, n, start + 1);
+		a[start] = 0;
+	}
+}
+void sinh(int a[], int n, int& final)
+{
+	int i = n - 1;
+	while (i >= 0 && a[i] == 1)
+	{
+		a[i] = 0;
+		i--;
+	}
+	if (i == -1)
+	{
+		final = 1;
+	}
+	else
+	{
+		a[i] = 1;
 	}
 }
 int main()
 {
-	int n, k; cin >> n >> k;
-	int a[1000];
-	for (int i = 0; i < n; i++)
+	int a[100] = { 0 };
+	string name[100] = { "Tom", "Jerry", "Jack" };
+	int final = 0;
+	int n;
+	cin >> n;
+	while (final == 0)
 	{
-		cin >> a[i];
+		for (int i = 0;i < n; i++)
+		{
+			if (a[i] == 1)
+			{
+				cout << name[i] << " ";
+			}
+		}
+		cout << endl;
+		sinh(a, n, final);
 	}
-	int partition[100] = { 0 };
-	int max_min = INT_MAX;
-	BackTrack(a, partition, n, k, max_min);
-	cout << max_min;
 }
