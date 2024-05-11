a8bc53bc4c1e7bf0ce82ea3f894d925c9d11dfac
Nguyen Duc Tri <ductri0981@gmail.com>
2024-05-11 19:07:18 +0700

    The horse petrol (Search google translate :)))

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -1,44 +1,77 @@
 #include<iostream>
 using namespace std;
 
-void Backtrack(int a[][100], char save[], int N, int indexX = 0, int indexY = 0, int cnt = 0)
+int dx[8] = { -2, -2, -1, -1, 1, 1, 2, 2 };
+int dy[8] = { -1, 1, -2, 2, -2, 2, -1, 1 };
+void HorsePatrol(int a[100][100], int N, int indexX = 0, int indexY = 0, int count = 0)
 {
-	if (indexX == N - 1 && indexY == N - 1)
+	if (count == N * N - 1)
 	{
-		for (int i = 0; i < cnt; i++)
-		{
-			cout << save[i];
-		}
-		cout << "   ";
-		return;
-	}
-	//Go down
-	if (indexX + 1 >= 0 && indexX + 1 < N && a[indexX + 1][indexY] == 1)
-	{
-		save[cnt] = 'D';
-		Backtrack(a, save, N, indexX + 1, indexY, cnt + 1);
-	}
-	if (indexY + 1 >= 0 && indexY + 1 < N && a[indexX][indexY + 1] == 1)
-	{
-		save[cnt] = 'R';
-		Backtrack(a, save, N, indexX, indexY + 1, cnt + 1);
-	}
-}
-int main()
-{
-	int t; cin >> t;
-	while (t--)
-	{
-		int N; cin >> N;
-		int a[100][100];
 		for (int i = 0; i < N; i++)
 		{
 			for (int j = 0; j < N; j++)
 			{
-				cin >> a[i][j];
+				cout << a[i][j] << " ";
+			}
+			cout << endl;
+		}
+		cout << endl;
+		system("pause");
+		return;
+	}
+	for (int i = 0; i < 8; i++)
+	{
+		int i1 = indexX + dx[i];
+		int j1 = indexY + dy[i];
+		if (i1 >= 0 && i1 < N && j1 >= 0 && j1 < N && a[i1][j1] == 0)
+		{
+			a[i1][j1] = count + 2;
+			HorsePatrol(a, N, i1, j1, count + 1);
+			a[i1][j1] = 0;
+		}
+	}
+}
+void HorsePatrolAdvance(int a[100][100], int N, int indexX = 0, int indexY = 0, int count = 0)
+{
+	if (count == N * N - 1)
+	{
+		for (int i = 0; i < 8; i++)
+		{
+			int i1 = indexX + dx[i];
+			int j1 = indexY + dy[i];
+			if (i1 >= 0 && i1 < N && j1 >= 0 && j1 < N && a[i1][j1] == 1)
+			{
+				for (int i = 0; i < N; i++)
+				{
+					for (int j = 0; j < N; j++)
+					{
+						cout << a[i][j] << " ";
+					}
+					cout << endl;
+				}
+				cout << endl;
+				system("pause");
+				break;
 			}
 		}
-		char save[100];
-		Backtrack(a, save, N);
+		return;
 	}
+	for (int i = 0; i < 8; i++)
+	{
+		int i1 = indexX + dx[i];
+		int j1 = indexY + dy[i];
+		if (i1 >= 0 && i1 < N && j1 >= 0 && j1 < N && a[i1][j1] == 0)
+		{
+			a[i1][j1] = count + 2;
+			HorsePatrolAdvance(a, N, i1, j1, count + 1);
+			a[i1][j1] = 0;
+		}
+	}
+}
+int main()
+{
+	int N; cin >> N;
+	int a[100][100] = { 0 };
+	a[0][0] = 1;
+	HorsePatrolAdvance(a, N);
 }
