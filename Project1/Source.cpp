30ebd93429f346274cc051a167b39d33eafdc730
Nguyen Duc Tri <ductri0981@gmail.com>
2024-05-11 16:55:30 +0700

    Di chuyen trong me cung

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -1,31 +1,44 @@
 #include<iostream>
 using namespace std;
 
-void Try(int a[], int mask[], int n, int k, int start = 0)
+void Backtrack(int a[][100], char save[], int N, int indexX = 0, int indexY = 0, int cnt = 0)
 {
-	if (start == k)
+	if (indexX == N - 1 && indexY == N - 1)
 	{
-		for (int i = 0; i < k; i++)
+		for (int i = 0; i < cnt; i++)
 		{
-			cout << a[i];
+			cout << save[i];
 		}
-		cout << endl;
+		cout << "   ";
 		return;
 	}
-	for (int i = 1; i <= n; i++)
+	//Go down
+	if (indexX + 1 >= 0 && indexX + 1 < N && a[indexX + 1][indexY] == 1)
 	{
-		if (mask[i] == 0)
-		{
-			mask[i] = 1;
-			a[start] = i;
-			Try(a, mask, n, k, start + 1);
-			mask[i] = 0;
-		}
+		save[cnt] = 'D';
+		Backtrack(a, save, N, indexX + 1, indexY, cnt + 1);
+	}
+	if (indexY + 1 >= 0 && indexY + 1 < N && a[indexX][indexY + 1] == 1)
+	{
+		save[cnt] = 'R';
+		Backtrack(a, save, N, indexX, indexY + 1, cnt + 1);
 	}
 }
 int main()
 {
-	int x[100] = { 0 };
-	int mask[100] = { 0 };
-	Try(x, mask, 5, 3);
+	int t; cin >> t;
+	while (t--)
+	{
+		int N; cin >> N;
+		int a[100][100];
+		for (int i = 0; i < N; i++)
+		{
+			for (int j = 0; j < N; j++)
+			{
+				cin >> a[i][j];
+			}
+		}
+		char save[100];
+		Backtrack(a, save, N);
+	}
 }
