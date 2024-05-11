f41ff9bb74be1f9cbe5f67bb6fd4d566cb96ceb7
Nguyen Duc Tri <ductri0981@gmail.com>
2024-05-11 15:55:03 +0700

    Hoan vi cac so

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -1,61 +1,31 @@
 #include<iostream>
-#include<algorithm>
-#include<vector>
-#include<set>
 using namespace std;
 
-void backtrack(int a[], int n, int start = 0)
+void Try(int x[100], int mask[100], int n, int start = 0)
 {
 	if (start == n)
 	{
 		for (int i = 0; i < n; i++)
 		{
-			cout << a[i];
+			cout << x[i];
 		}
 		cout << endl;
 		return;
 	}
-	for (int i = 0; i <= 1; i++)
+	for (int i = 1; i <= n; i++)
 	{
-		a[start] = i;
-		backtrack(a, n, start + 1);
-		a[start] = 0;
-	}
-}
-void sinh(int a[], int n, int& final)
-{
-	int i = n - 1;
-	while (i >= 0 && a[i] == 1)
-	{
-		a[i] = 0;
-		i--;
-	}
-	if (i == -1)
-	{
-		final = 1;
-	}
-	else
-	{
-		a[i] = 1;
+		if (mask[i] == 0)
+		{
+			mask[i] = 1;
+			x[start] = i;
+			Try(x, mask, n, start + 1);
+			mask[i] = 0;
+		}
 	}
 }
 int main()
 {
-	int a[100] = { 0 };
-	string name[100] = { "Tom", "Jerry", "Jack" };
-	int final = 0;
-	int n;
-	cin >> n;
-	while (final == 0)
-	{
-		for (int i = 0;i < n; i++)
-		{
-			if (a[i] == 1)
-			{
-				cout << name[i] << " ";
-			}
-		}
-		cout << endl;
-		sinh(a, n, final);
-	}
+	int x[100] = { 0 };
+	int mask[100] = { 0 };
+	Try(x, mask, 5);
 }
