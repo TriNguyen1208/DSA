1db6f5c1981f0ca8644ead056bdeab5153daa8e8
Nguyen Duc Tri <ductri0981@gmail.com>
2024-05-15 00:22:58 +0700

    Dictionary

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -1,45 +1,86 @@
 #include<iostream>
+#include<string>
 using namespace std;
 
-bool IsDescending(int a[], int n)
+void BackTrack(string dic[], char A[][10], int K, int M, int N, int word, int indexX, int indexY, int cnt = 1)
 {
-	for (int i = 0; i < n - 1; i++)
+	if (cnt == dic[word].length())
 	{
-		if (a[i + 1] > a[i])
-		{
-			return false;
-		}
-	}
-	return true;
-}
-void backtrack(int a[], int n, int sum = 0, int cnt = 0)
-{
-	if (sum == n)
-	{
-		if (IsDescending(a, cnt) == true)
-		{
-			for (int i = 0; i < cnt; i++)
-			{
-				cout << a[i] << " ";
-			}
-			cout << endl;
-		}
+		cout << dic[word] << " ";
 		return;
 	}
-	for (int i = 5; i > 0; i--)
+	//Cheo Trai
+	if (indexX - 1 >= 0 && indexY - 1 >= 0 && A[indexX - 1][indexY - 1] == dic[word][cnt])
 	{
-		if (sum + i <= n)
+		BackTrack(dic, A, K, M, N, word, indexX - 1, indexY - 1, cnt + 1);
+	}
+	//Cheo Phai
+	if (indexX - 1 >= 0 && indexY + 1 < N && A[indexX - 1][indexY + 1] == dic[word][cnt])
+	{
+		BackTrack(dic, A, K, M, N, word, indexX - 1, indexY + 1, cnt + 1);
+	}
+	//Cheo Trai Xuong
+	if (indexX + 1 < M && indexY - 1 >= 0 && A[indexX + 1][indexY - 1] == dic[word][cnt])
+	{
+		BackTrack(dic, A, K, M, N, word, indexX + 1, indexY - 1, cnt + 1);
+	}
+	//Cheo Phai Xuong
+	if (indexX + 1 < M && indexY + 1 < N && A[indexX + 1][indexY + 1] == dic[word][cnt])
+	{
+		BackTrack(dic, A, K, M, N, word, indexX + 1, indexY + 1, cnt + 1);
+	}
+	// Tren
+	if (indexX - 1 >= 0 && A[indexX - 1][indexY] == dic[word][cnt])
+	{
+		BackTrack(dic, A, K, M, N, word, indexX - 1, indexY, cnt + 1);
+	}
+	//Xuong
+	if (indexX + 1 < M && A[indexX + 1][indexY] == dic[word][cnt])
+	{
+		BackTrack(dic, A, K, M, N, word, indexX + 1, indexY, cnt + 1);
+	}
+	//Phai
+	if (indexY - 1 >= 0 && A[indexX][indexY - 1] == dic[word][cnt])
+	{
+		BackTrack(dic, A, K, M, N, word, indexX, indexY - 1, cnt + 1);
+	}
+	//Trai
+	if (indexY + 1 < N && A[indexX][indexY + 1] == dic[word][cnt])
+	{
+		BackTrack(dic, A, K, M, N, word, indexX, indexY + 1, cnt + 1);
+	}
+}
+void Search(string dic[], char A[][10], int K, int M, int N, int word)
+{
+	for (int i = 0; i < M; i++)
+	{
+		for (int j = 0; j < N; j++)
 		{
-			a[cnt] = i;
-			backtrack(a, n, sum + i, cnt + 1);
-			a[cnt] = 0;
+			if (A[i][j] == dic[word][0])
+			{
+				BackTrack(dic, A, K, M, N, word, i, j);
+			}
 		}
 	}
 }
 int main()
 {
-	int n;
-	cin >> n;
-	int a[100];
-	backtrack(a, n);
+	int K, M, N; cin >> K >> M >> N;
+	string dic[100];
+	for (int i = 0; i < K; i++)
+	{
+		cin >> dic[i];
+	}
+	char A[10][10];
+	for (int i = 0; i < M; i++)
+	{
+		for (int j = 0; j < N; j++)
+		{
+			cin >> A[i][j];
+		}
+	}
+	for (int i = 0; i < K; i++)
+	{
+		Search(dic, A, K, M, N, i);
+	}
 }
